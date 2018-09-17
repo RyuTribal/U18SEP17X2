c7fa8af88bcf9b0e1d35638f98d7d902f2a8b6f4
RyuTribal <ivan.sedelkin9@gmail.com>
2018-09-17 22:07:29 +0200

    Added list

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> cards = new List<int>();
+            int ammount = 52;
+            for (int i = 1; i < ammount; i++)
+            {
+                cards.Add(i);
+            }
+
+            
+        }
+    }
+}
