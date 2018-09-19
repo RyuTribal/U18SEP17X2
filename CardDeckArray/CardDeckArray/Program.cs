27ecdb640e29ab314b78289c730936308883b184
RyuTribal <ivan.sedelkin9@gmail.com>
2018-09-19 13:53:20 +0200

    First assigment of B with jagged Lists

---
+++ CardDeckArray/CardDeckArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardDeckArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> deck = new List<string[]>();
+            deck.Add(new string[] = {"Ess","Två","Tre","Fyra","Fem","Sex","Sju","Åtta","Nio","Tio","Knäckt","Dam","Kung" });
+
+
+
+        }
+    }
+}
